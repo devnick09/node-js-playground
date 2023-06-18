b4a36ee1408e2e1e19069272f75746dba1b21d6e
Nikhil Sharma <nikhil@gkpwebsolutions.com>
2023-06-18 20:38:31 +0530

    added layout to pages

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayout = require('express-ejs-layouts');
+
+app.use(expressLayout);
 
 // use express router
 app.use('/', require('./routes'));
