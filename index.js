55c490c9614c569387570f0d16f54aaedeb920fd
Nikhil Sharma <nikhil@gkpwebsolutions.com>
2023-06-18 21:01:52 +0530

    added static files

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express();
 const port = 8000;
 const expressLayout = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+
 app.use(expressLayout);
+// extract style and script from sub pages
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 // use express router
 app.use('/', require('./routes'));
