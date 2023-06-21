e0fb237c80c4b3f0e1e742d99dfdcf789fda1f4c
Nikhil Sharma <nikhil@gkpwebsolutions.com>
2023-06-21 23:07:07 +0530

    manual authentication implemented

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,39 @@
+const User = require('../models/users')
+
 module.exports.users = function(req, res){
     return res.render('users', {
         title: "Users",
     });
+}
+
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: "Sign Up",
+    });
+}
+
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "Sign In",
+    });
+}
+
+module.exports.createUser =async function(req, res){
+    if (req.body.password != req.body.confirm_password) {
+        return res.redirect('back');
+    }
+
+    let matchingUser = await User.findOne({email : req.body.email});
+
+    if (matchingUser != null) {
+        console.log("Email is already existing in DB");
+        return res.redirect('back');
+    }else{
+        // crating new User and storing into DB
+        User.create(req.body);
+        return res.redirect('/users/sign-in');
+    }
+}
+module.exports.createSession = function(req, res){
+    // get the user
 }
