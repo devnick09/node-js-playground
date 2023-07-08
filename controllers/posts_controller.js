4e00c345742211883638ae6fff4414bfff0462a8
Nikhil Sharma <nikhil@gkpwebsolutions.com>
2023-07-08 23:11:08 +0530

    delete action added to posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
+const { request } = require('express');
 
 module.exports.create = function(req, res){
     // console.log(req.user);
@@ -8,4 +10,17 @@ module.exports.create = function(req, res){
     });
 
     return res.redirect('/');
+}
+
+module.exports.destroy = async function(req, res){
+    var post = await Post.findById(req.params.id);
+
+    // .id means converting the object id to a string
+    if(post.user == req.user.id){
+        await Post.deleteOne({_id: req.params.id});
+        await Comment.deleteMany({post: req.params.id})
+        return res.redirect('back');
+    }else{
+        return res.redirect('back');
+    }
 }
